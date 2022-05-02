1653c1397e0f097f8706cd013067be29d62b2d53
Hudson Baroni <hudsonbarni@gmail.com>
2022-05-01 23:25:57 -0300

    Animaçãoes com JS

---
+++ js.js
@@ -0,0 +1,7 @@
+
+function aboutArrow() {
+    let arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    let about = document.getElementById('repositorys');
+    about.classList.toggle('showRepositys')
+}
