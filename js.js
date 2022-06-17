898bb2c1c7959dbcf64c55ba13cf2a46ecc1d779
Hudson Baroni <hudsonbarni@gmail.com>
2022-06-17 01:19:32 -0300

    Ajustes com API GitHub

--- js.js
+++ js.js
@@ -1,4 +1,3 @@
-
 function aboutArrow() {
     let arrow = document.getElementById('animation');
     arrow.classList.toggle('rotate');
@@ -6,3 +5,33 @@ function aboutArrow() {
     about.classList.toggle('showRepositys')
 }
 
+// AXIOS
+
+let arr = [];
+
+function repositorios() {
+    axios.get(`https://api.github.com/users/hudsonb1/repos`)
+        .then(response => {
+            const repositories = response.data;
+
+            repositories.map((repository) => {
+                arr.push({
+                    name: repository.name,
+                    url: repository.html_url
+                })
+            });
+            // console.log(arr);
+
+            const list = document.getElementById("list");
+
+            arr.map((item) => {
+                let a = document.createElement("a");
+                a.href = item.url;
+                a.target = '_blank';
+                a.innerHTML = item.name.toUpperCase();
+                list.appendChild(a);
+            });
+        }).catch(error => console.error(error));
+};
+
+repositorios();
